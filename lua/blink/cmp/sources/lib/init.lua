868c6d6e20dbde467a58a263d3187df295eadb7d
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-09-07 21:13:24 -0700

    fix: override lsp `dynamicRegistration` to be false

--- lua/blink/cmp/sources/lib/init.lua
+++ lua/blink/cmp/sources/lib/init.lua
@@ -292,6 +292,7 @@ function sources.get_lsp_capabilities(override, include_nvim_defaults)
   return vim.tbl_deep_extend('force', include_nvim_defaults and vim.lsp.protocol.make_client_capabilities() or {}, {
     textDocument = {
       completion = {
+        dynamicRegistration = false,
         completionItem = {
           snippetSupport = true,
           commitCharactersSupport = false, -- TODO:
